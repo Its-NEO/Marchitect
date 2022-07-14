46c2847835850542fe7dcaf9410d95118e0f7524
intellekt <com.intellekt@gmail.com>
2022-07-14 13:29:13 +0530

    Initial commit with only the source code

---
+++ cli.py
@@ -0,0 +1,202 @@
+import os
+import sys
+import re
+
+import model
+from tabulate import tabulate
+import json
+
+
+class Cli:
+    def __init__(self):
+        # messages
+        with open('strings.json') as f:
+            contents = json.load(f)
+            self.error_codes, self.help_codes = contents['error_codes'], contents['help']
+
+        # misc
+        self.model = model.Model()
+        self.mod_context = []
+        self.command = None
+
+    def help(self):
+        pass
+
+    def list(self):
+        if not self.model.mods:
+            print(self.error_codes['1'])
+            return
+
+        headers = ['Index', 'Mod', 'Description', 'Downloads']
+        mods = []
+        for index, mod in enumerate(self.model.mods):
+            mods.append([index + 1, mod['name'], mod['summary'], mod['downloads']])
+
+        # noinspection PyArgumentList
+        print('\n' + tabulate(mods, headers=headers, tablefmt='fancy_grid', maxcolwidths=[None, None, 40, None]) + '\n')
+
+    @staticmethod
+    def clear():
+        os.system('cls')
+
+    def state(self):
+        mod_loader, game_version = self.model.get_state()
+        print('\nMod Loader =', mod_loader)
+        print('Game Version =', game_version, '\n')
+
+    @staticmethod
+    def exit():
+        sys.exit()
+
+    def reset(self):
+        self.model.reset()
+        print('\nMarchitect has been reset to its defaults!\n')
+
+    def set(self):
+        res = re.search(r'^set (mod_loader (?:forge|fabric)|game_version [\d.]+)$', self.command)
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        option, val = res.group(1).split()
+
+        if option == "mod_loader":
+            self.model.set_mod_loader(val)
+        else:
+            self.model.set_game_version(val)
+
+        print('\nYour options were updated successfully!\n')
+
+    def search(self):
+        res = re.search(r"^search ([\w ',.]+$)", self.command)
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        results = self.model.search_call(res.group(1))
+
+        if not results:
+            print("\nYour search did not return any results :(\n")
+            return
+
+        self.mod_context = results
+
+        headers = ['Index', 'Mod', 'Description', 'Downloads']
+        mods = []
+        for index, mod in enumerate(self.mod_context):
+            mods.append([index + 1, mod['name'], mod['summary'], mod['downloads']])
+
+        # noinspection PyArgumentList
+        print('\n' + tabulate(mods, headers=headers, tablefmt='fancy_grid', maxcolwidths=[None, None, 40, None]) + '\n')
+
+    def add(self):
+        res = re.search(r'^add (\d)$', self.command)
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        index = res.group(1)
+
+        if not self.mod_context:
+            print(self.error_codes['4'])
+            return
+
+        if int(index) < 1 or int(index) > len(self.mod_context):
+            print(self.error_codes['3'])
+            return
+
+        self.model.add_mod(self.mod_context[int(index) - 1])
+        print('\nMod added successfully!\n')
+
+    def remove(self):
+        res = re.search(r'^remove (\d)$', self.command)
+
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        index = res.group(1)
+
+        if not self.model.mods:
+            print(self.error_codes['1'])
+            return
+
+        if int(index) < 1 or int(index) > len(self.model.mods):
+            print(self.error_codes['3'])
+            return
+
+        del self.model.mods[int(index) - 1]
+        print('\nMod removed successfully!\n')
+
+    def export(self):
+        res = re.search(r'^export "?([A-Z]:[\\|/](?:[\w_\-. ]+[\\|/])*)([\w_\-. ]+\.txt)"?$', self.command)
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        if not os.path.exists(res.group(1)):
+            print(self.error_codes['5'])
+            return
+
+        path = os.path.join(res.group(1), res.group(2))
+        self.model.export_file(path)
+
+        print(f'\nMods exported at {path} successfully!\n')
+
+    def download(self):
+        res = re.search(r'^download "?([A-Z]:[\\|/](?:[\w_\-. ]+[\\|/])*[\w_\-. ]+\.txt)"? -o "?([A-Z]:[\\|/](?:['
+                        r'\w_\-. ]+[\\|/])*[\w_\-. ]+)[\\|/]?"?$', self.command)
+
+        if res is None:
+            print(self.error_codes['2'])
+            return
+
+        if not os.path.exists(res.group(1)):
+            print(self.error_codes['6'])
+            return
+
+        if not os.path.exists(res.group(2)):
+            print(self.error_codes['5'])
+            return
+
+        self.model.download_mods(res.group(1), res.group(2))
+        print(f'\nThe mods has been downloaded successfully and saved at {res.group(2)}!\n')
+
+    def run(self):
+        # noinspection PyUnresolvedReferences
+        command_list = {
+            'help': self.help,
+            'list': self.list,
+            'clear': self.clear,
+            'state': self.state,
+            'exit': self.exit,
+            'reset': self.reset,
+            'set': self.set,
+            'search': self.search,
+            'add': self.add,
+            'remove': self.remove,
+            'export': self.export,
+            'download': self.download
+        }
+
+        print()
+
+        while True:
+            inp = input('>>> ')
+
+            if inp == '':
+                continue
+
+            if inp.split()[0] in command_list.keys():
+                self.command = inp
+                command_list.get(self.command.split()[0])()
+
+
+if __name__ == '__main__':
+    c = Cli()
+
+    '''c.model.add_mod(c.model.search_call('pixelmon')[0])
+    c.model.add_mod(c.model.search_call('antique atlas')[0])
+    c.model.add_mod(c.model.search_call('appleskin')[0])'''
+
+    c.run()
---
+++ model.py
@@ -0,0 +1,198 @@
+import ast
+import itertools
+import os.path as path
+import time
+from threading import Thread
+
+import requests
+
+
+class Model:
+    def __init__(self):
+        # mod list
+        self.mods = []
+
+        # mod options (all set to defaults)
+        self._game_version = '1.12.2'
+        self._mod_loader = 'forge'
+        self._sort_field = 2
+        self._sort_order = 'desc'
+
+        # constants
+        self._GAME_ID = 432
+        self._BASE_URL = 'https://api.curseforge.com'
+        self._HEADERS = {
+            'Accept': 'application/json',
+            'x-api-key': '$2a$10$j6qk.vRJ.SpjbWV598I69eovD/eJoKvGT.0BlY7l6karlNzrl5KY2'
+        }
+
+    def set_game_version(self, game_version):
+        self._game_version = game_version
+
+    def set_mod_loader(self, mod_loader):
+        self._mod_loader = mod_loader
+
+    def add_mod(self, mod_obj: dict):
+        self.mods.append(mod_obj)
+
+    def remove_mod(self, mod_obj: dict):
+        self.mods.remove(mod_obj)
+
+    def get_state(self):
+        return self._mod_loader, self._game_version
+
+    def reset(self):
+        self.mods = []
+        self.set_game_version('1.12.2')
+        self.set_mod_loader('forge')
+
+    def search_call(self,
+                    search_filter: str,
+                    ) -> list:
+
+        """
+        Searches a mod with all the given parameters
+        :param search_filter:   Search term
+        :return:                [{
+                                    'logo': string,
+                                    'slug': string,
+                                    'name': string,
+                                    'summary': string,
+                                    'authors': [string],
+                                    'downloads': int,
+                                    'categories': [{
+                                        'name': string,
+                                        'icon_url': string
+                                    }],
+                                    'size': int,
+                                    'files': [{
+                                        'fingerprint': int,
+                                        'file_name': string,
+                                        'download_url': string
+                                    }],
+                                }]
+        """
+
+        mod_loader_enum = {
+            'forge': 1,
+            'fabric': 4
+        }
+
+        # Search call
+        data = requests.get(self._BASE_URL + '/v1/mods/search', headers=self._HEADERS, params={
+            'gameId': self._GAME_ID,
+            'gameVersion': self._game_version,
+            'searchFilter': search_filter,
+            'sortField': self._sort_field,
+            'sortOrder': self._sort_order,
+            'modLoaderType': mod_loader_enum[self._mod_loader]
+        }).json()['data']
+
+        # manufacturing the result
+        result = [{
+            'mod_id': d['id'],
+            'name': d['name'],
+            'summary': d['summary'],
+            'authors': [author['name'] for author in d['authors']],
+            'downloads': d['downloadCount'],
+            'categories': [c['name'] for c in d['categories']],
+            'files': [{
+                'file_name': f['filename'],
+                'game_version': f['gameVersion'],
+                'file_id': f['fileId'],
+                'mod_loader': f['modLoader']
+            } for f in d['latestFilesIndexes'] if self._game_version in f['gameVersion']],
+            'usable': True if d['latestFilesIndexes'][0]['filename'].endswith('jar') else False
+        } for d in data]
+
+        # sorting with mods with user selected priority if both forge and fabric versions available on same mod
+        for element in result:
+            new_mods = []
+            for i, e in enumerate(element['files']):
+                if mod_loader_enum.get(self._mod_loader) == e['mod_loader']:
+                    new_mods.append(e)
+            element['files'] = new_mods.copy()
+
+        # Removing all the mods with empty files and mod packs
+        result = [res for res in result if res['files'] and res['usable']]
+
+        return result
+
+    def export_file(self, out_path):
+        """
+        Exports a Marchitect file with all the necessary info to download the mods
+        :param out_path:        File path of the (to be)exported file
+        :return:                Exit codes
+        """
+
+        # removing any duplicate mods before exporting
+        new_mods = []
+        for element in self.mods:
+            if element['mod_id'] not in [file['mod_id'] for file in new_mods]:
+                new_mods.append(element)
+        self.mods = new_mods
+
+        # writing each mod with a new line
+        with open(out_path, 'w') as file:
+            for elem in self.mods:
+                file.write(str(elem) + '\n')
+                file.flush()
+
+    def download_mods(self, src_file, out_dir):
+        """
+        Download all the mods from a Marchitect file
+        :param src_file:        Source of the Marchitect file
+        :param out_dir:         A directory to populate with downloaded mods
+        :return:                True if successful, False if not
+        """
+
+        print("Downloading:")
+        # Formatting contents of the file to dict Format
+        with open(src_file) as file:
+            mod_list = file.readlines()
+        mod_list = [ast.literal_eval(mod.strip()) for mod in mod_list]
+
+        is_done = False
+
+        def _animate(*mod_name):
+            for c in itertools.cycle(['|', '/', '-', '\\']):
+                if is_done:
+                    break
+                print(f'{mod_name[0]}...{c}', end='\r')
+                time.sleep(0.1)
+
+        # downloading the mods
+        for mod in mod_list:
+            is_done = False
+            animation_thread = Thread(target=_animate, args=[mod["name"]])
+            animation_thread.start()
+
+            # extracting mod info [first file to be taken]
+            mod_file = mod['files'][0]
+            mod_id, filename, file_id = mod['mod_id'], mod_file['file_name'], mod_file['file_id']
+
+            # skipping already existing mods
+            if path.exists(path.join(out_dir, filename)):
+                is_done = True
+                animation_thread.join()
+                print(f'{mod["name"]} - Skipped')
+                continue
+
+            # decoding file url after getting it
+            file_url = requests.get(self._BASE_URL + f'/v1/mods/{mod_id}/files/{file_id}/download-url',
+                                    headers=self._HEADERS)
+            if file_url.content == b'':
+                is_done = True
+                animation_thread.join()
+                print(f'{mod["name"]} - ERROR')  # <- if no download url found
+                continue
+
+            file_url = ast.literal_eval(file_url.content.decode())['data']
+            r = requests.get(file_url, stream=True)
+            with open(path.join(out_dir, filename), 'wb') as file:
+                for chunk in r.iter_content(1024):
+                    file.write(chunk)
+
+            is_done = True
+            animation_thread.join()
+            print(f'{mod["name"]} - Done!')
